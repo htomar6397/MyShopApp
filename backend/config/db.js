01a4325428fc0ad043aa0544e418f2e87155b7f5
htomar6397 <126243160+htomar6397@users.noreply.github.com>
2023-12-22 19:51:49 +0530

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
  const connectDB = async () => {
    try {
-     const conn = await mongoose.connect("mongodb://127.0.0.1:27017/ecom");
+     const conn = await mongoose.connect(
+       process.env.ENVIROMENT === "production"
+         ? process.env.MONGO_URI
+         : "mongodb://127.0.0.1:27017/ECOM"
+     );
      console.log(`MongoDB Connected: ${conn.connection.host}`);
    } catch (error) {
      console.error(`Error: ${error.message}`);
