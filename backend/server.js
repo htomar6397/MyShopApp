bb10c18fb2db910dcf80bc6a40cd1f5907eb3c5a
Mayank Tomar <126243160+htomar6397@users.noreply.github.com>
2025-01-31 00:02:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -58,10 +58,10 @@ app.use("/api/orders", orderRoutes);
 
 if (process.env.ENVIROMENT === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "build", "index.html"))
   );
 }
 
